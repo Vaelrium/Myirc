fecea6f4d96d001c0bdc9adfa254bbb7fb6dabb1
coutar-a <AGCoutarel@gmail.com>
2015-04-09 16:28:10 +0200

    mergin

--- server_dir/cmds_handler.c
+++ server_dir/cmds_handler.c
@@ -5,10 +5,31 @@
 ** Login   <durand_u@epitech.net>
 ** 
 ** Started on  Thu Apr  9 13:59:06 2015 Rémi DURAND
-** Last update Thu Apr  9 14:13:57 2015 Rémi DURAND
+** Last update Thu Apr  9 15:01:45 2015 Ambroise Coutarel
 */
 
 #include "irc.h"
+
+t_command	g_commands_server[8];
+
+void	subinit(t_command *cmd, char *name,
+		int (*func)(t_cfds*, char**, int))
+{
+  cmd->command = strdup(name);
+  cmd->func = func;
+}
+
+void	init_commands()
+{
+  subinit(&(g_commands_server[0]), "/nick", &nick);
+  subinit(&(g_commands_server[1]), "/list", &list);
+  subinit(&(g_commands_server[2]), "/join", &join);
+  /* subinit(&(g_commands_server[3]), "/part", &part); */
+  /* subinit(&(g_commands_server[4]), "/users", &users); */
+  /* subinit(&(g_commands_server[5]), "/msg", &msg); */
+  /* subinit(&(g_commands_server[6]), "/send_file", &send_file); */
+  /* subinit(&(g_commands_server[7]), "/accept_file", &accept_file); */
+}
 
 void		handle_cmds(t_cfds *e, char **cmd, int fd)
 {
@@ -17,6 +38,18 @@ void		handle_cmds(t_cfds *e, char **cmd, int fd)
   (void)e;
   (void)fd;
   v = 0;
-  while (cmd[v])
-    printf("%s\n", cmd[v++]);
+  init_commands();
+  /* while (cmd[v]) */
+  /*   printf("%s\n", cmd[v++]); */
+  while (v != CMD_SERV)
+    {
+      if (strcmp(cmd[0], g_commands_server[v].command) == 0)
+	{
+	  g_commands_server[v].func(e, cmd, fd);
+	  free_wtab(cmd);
+	  return ;
+	}
+      ++v;
+      //free_wtab(cmd);
+    }
 }
--- server_dir/irc.h
+++ server_dir/irc.h
@@ -5,7 +5,7 @@
 ** Login   <durand_u@epitech.net>
 ** 
 ** Started on  Mon Apr  6 12:39:14 2015 Rémi DURAND
-** Last update Thu Apr  9 14:11:02 2015 Rémi DURAND
+** Last update Thu Apr  9 16:08:00 2015 Ambroise Coutarel
 */
 
 #ifndef _IRC_H_
@@ -31,6 +31,25 @@
 # define FD_FREE 0
 # define FD_CLIENT 1
 # define FD_SERVER 2
+# define CMD_SERV 3 // à augmenter au fur et à mesure des implementations
+
+/*
+** NICK
+*/
+
+# define ERR_NONICKNAMEGIVEN "431" // pas de paramètre pour /nick
+# define ERR_ERRONEUSNICKNAME "432" // nickname contient chars invalides
+# define ERR_NICKNAMEINUSE "433" //
+# define ERR_NICKCOLLISION "436" // redondant avec nicknameinuse ?
+# define ERR_NEEDMOREPARAMS "461"
+# define ERR_INVITEONLYCHAN "743"
+# define ERR_CHANNELISFULL "471"
+# define ERR_NOSUCHCHANNEL "403"
+# define ERR_BANNEDFROMCHAN "474"
+# define ERR_BADCHANNELKEY "475"
+# define ERR_BADCHANMASK "476"
+# define ERR_TOOMANYCHANNELS "405"
+# define ERR_NOTONCHANNEL "442"
 
 typedef const struct sockaddr *	caddr_c;
 typedef struct sockaddr *	addr_c;
@@ -48,6 +67,12 @@ typedef struct	s_cfds
   int		ncfd;
 }		t_cfds;
 
+typedef struct		s_command
+{
+  char			*command;
+  int			(*func)(t_cfds *e, char **cmd, int fd);
+}			t_command;
+
 int		init_serv(struct protoent **pe, char **av,
 			  struct sockaddr_in *s_in);
 int		init_cli(t_cfds *cdata);
@@ -55,5 +80,11 @@ void		server_read(t_cfds *cdata, int fd, fd_set *set);
 void		server_write(t_cfds *cdata, int fd, fd_set writefds);
 void		handle_cmds(t_cfds *e, char **cmd, int fd);
 char		**my_str_to_wordtab(char *str);
+void	subinit(t_command *cmd, char *name,
+		int (*func)(t_cfds*, char**, int));
+void	init_commands();
+void	free_wtab(char **tab);
+
+extern	t_command	g_commands_server[8];
 
 #endif /* !_IRC_H_ */
